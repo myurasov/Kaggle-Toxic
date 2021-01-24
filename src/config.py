ca96b2aed0dabe08b945ccaba22c6add5ab0c23d
Misha Yurasov <me@yurasov.me>
2021-01-24 11:53:26 +0000

    Update

--- src/config.py
+++ src/config.py
@@ -5,6 +5,10 @@ config = {}
 # data location
 config["DATA_DIR"] = "/app/_data"
 
+# path to pre-trained bert at google storage
+# @see https://github.com/google-research/bert
+config["GS_BERT_MODEL_PATH"] = "2018_10_18/uncased_L-12_H-768_A-12"
+
 # max length of comments (chars)
 config["MAX_TEXT_LENGTH"] = 5120
 
--- src/download_bert.sh
+++ src/download_bert.sh
@@ -2,11 +2,9 @@
 
 # download pre-trained BERT from gcloud
 
-DATA_DIR=`python -c "from src.config import config; print(config['DATA_DIR'])"`
+DATA_DIR=`python -c "import src.config import config as c; print(config['DATA_DIR'])"`
+MODEL_PATH=`python -c "from src.config import config; print(config['GS_BERT_MODEL_PATH'])"`
 DEST_DIR="${DATA_DIR}/bert"
-
-MODEL_DIR="2018_10_18"
-MODEL_NAME="uncased_L-12_H-768_A-12"
 
 rm -rfv $DEST_DIR
 mkdir -pv $DEST_DIR
@@ -15,6 +13,6 @@ cd $DEST_DIR
 for f in "bert_config.json" "vocab.txt" "bert_model.ckpt.meta" \
     "bert_model.ckpt.index" "bert_model.ckpt.data-00000-of-00001"
 do
-    cmd="gsutil cp gs://bert_models/${MODEL_DIR}/${MODEL_NAME}/${f} $DEST_DIR/${f}"
+    cmd="gsutil cp gs://bert_models/${MODEL_PATH}/${f} $DEST_DIR/${f}"
     $cmd
 done
