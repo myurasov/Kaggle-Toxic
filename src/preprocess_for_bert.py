548f14241596d910b45e32fba3f99c4d40cea898
Misha Yurasov <me@yurasov.me>
2021-01-24 02:24:38 +0000

    Update

--- src/preprocess_for_bert.py
+++ src/preprocess_for_bert.py
@@ -31,22 +31,17 @@ def preprocess_text_for_bert(tokenizer, text, max_text_len, max_seq_len):
 
 
 # mapping of comments->tokenized inputs
-def _comment_mapping_fn(text):
+def _comment_mapping(text):
     return preprocess_text_for_bert(
         tokenizer=tokenizer,
-        max_text_len=max_text_len,
-        max_seq_len=max_seq_len,
+        max_text_len=config["MAX_TEXT_LENGTH"],
+        max_seq_len=config["MAX_SEQ_LENGTH"],
         text=text,
     )
 
 
-tokenizer = FullTokenizer(vocab_file=config["DATA_DIR"] + "/bert/vocab.txt")
-max_text_len = config["MAX_TEXT_LENGTH"]
-max_seq_len = config["MAX_SEQ_LENGTH"]
-
-
-# process csv file
-def process_csv(csv_path):
+# convert csv file to numpy data
+def csv_to_npy(csv_path):
 
     df = pd.read_csv(csv_path)
     df = df.set_index("id")
@@ -58,7 +53,7 @@ def process_csv(csv_path):
         X = list(
             tqdm(
                 pool.imap(
-                    _comment_mapping_fn,
+                    _comment_mapping,
                     comments,
                 ),
                 total=len(df),
@@ -73,21 +68,21 @@ def process_csv(csv_path):
     return {"ids": ids, "X": X, "Y": Y}
 
 
-###
-
-output_dir = config["DATA_DIR"] + "/processsed"
-shutil.rmtree(output_dir, ignore_errors=True)
-os.makedirs(output_dir, exist_ok=True)
-
-
-def _process_csv(set_name):
+# process train/test dataset
+def _process_set(set_name):
     input_file = config["DATA_DIR"] + f"/src/{set_name}.csv"
     print(f"Processing {input_file}...")
-    processed_data = process_csv(input_file)
+    processed_data = csv_to_npy(input_file)
     output_file = output_dir + f"/{set_name}.npy"
     np.save(output_file, processed_data)
     print("Saved to", output_file)
 
 
-_process_csv("train")
-_process_csv("test")
+tokenizer = FullTokenizer(vocab_file=config["DATA_DIR"] + "/bert/vocab.txt")
+
+output_dir = config["DATA_DIR"] + "/processsed"
+shutil.rmtree(output_dir, ignore_errors=True)
+os.makedirs(output_dir, exist_ok=True)
+
+_process_set("train")
+_process_set("test")
